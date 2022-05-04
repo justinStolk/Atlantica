6bf2870fd78dfe8c5bf5cbf4635d47995d594710
Jogchum1 <jogchum1@hotmail.nl>
2022-05-04 19:03:51 +0200

    Added Swimming and Walking

---
+++ Assets/Scripts/MovementScripts/PlayerFSM.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFSM : MonoBehaviour
+{
+    private FSM stateMachine;
+    private void Start()
+    {
+        stateMachine = new FSM(typeof(WalkingState), GetComponents<BaseState>());
+    }
+    private void Update()
+    {
+        stateMachine.FSMUpdate();
+    }
+
+    private void FixedUpdate()
+    {
+        stateMachine.FSMFixedUpdate();
+    }
+}
