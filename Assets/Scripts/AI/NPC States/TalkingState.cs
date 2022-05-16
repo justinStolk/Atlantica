2ae485508e3db19893c7b7fb19ef847938c7e6b4
Justin <justinstolk97@gmail.com>
2022-05-16 16:45:00 +0200

    Minor AI behavioural tweaks
    Hopefully also a working implementation to interact with the AI

--- Assets/Scripts/AI/NPC States/TalkingState.cs
+++ Assets/Scripts/AI/NPC States/TalkingState.cs
@@ -1,20 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Yarn.Unity;
 
+[RequireComponent(typeof(DialogueRunner))]
 public class TalkingState : BaseState
 {
-    public Transform Speaker;
-    public bool IsTalking;
+    [HideInInspector] public Transform Speaker;
+    [HideInInspector] public bool IsTalking;
 
+    private DialogueRunner dialogueRunner;
     private BaseState previousState;
+
+    private void Start()
+    {
+        dialogueRunner = GetComponent<DialogueRunner>();
+    }
     public override void OnStateEnter()
     {
+        dialogueRunner.onDialogueComplete.AddListener(OnDialogueConcluded);
         IsTalking = true;
         previousState = owner.PreviousState;
         Vector3 speakerPos = new Vector3(Speaker.position.x, 0, Speaker.position.z);
         Vector3 myPos = new Vector3(transform.position.x, 0, transform.position.z);
         Vector3.RotateTowards(transform.position, Speaker.position, Mathf.Deg2Rad * 360, 90);
+        dialogueRunner.StartDialogue(dialogueRunner.startNode);
     }
 
     public override void OnStateExit()
@@ -31,8 +41,12 @@ public class TalkingState : BaseState
     {
         if (!IsTalking)
         {
+            dialogueRunner.onDialogueComplete.RemoveListener(OnDialogueConcluded);
             owner.SwitchState(previousState.GetType());
         }
     }
-
+    private void OnDialogueConcluded()
+    {
+        IsTalking = false;
+    }
 }
