40b394c90821e615244669c4d3e32ea76617057b
Justin <justinstolk97@gmail.com>
2022-05-20 20:03:13 +0200

    Reflector fix
    I think it does work, it has to

--- Assets/Scripts/Puzzles/HorizontalReflector.cs
+++ Assets/Scripts/Puzzles/HorizontalReflector.cs
@@ -5,6 +5,6 @@ public class HorizontalReflector : Reflector
     public override void Interact()
     {
         stanceIndex = (stanceIndex + 1) % stanceRotationValues.Length;
-        transform.SetPositionAndRotation(transform.position, Quaternion.Euler( new Vector3(0, stanceRotationValues[stanceIndex], 0)));
+        transform.SetPositionAndRotation(transform.position, Quaternion.Euler( new Vector3(transform.eulerAngles.x, stanceRotationValues[stanceIndex], transform.eulerAngles.z)));
     }
 }
--- Assets/Scripts/Puzzles/VerticalReflector.cs
+++ Assets/Scripts/Puzzles/VerticalReflector.cs
@@ -5,6 +5,6 @@ public class VerticalReflector : Reflector
     public override void Interact()
     {
         stanceIndex = (stanceIndex + 1) % stanceRotationValues.Length;
-        transform.SetPositionAndRotation(transform.position, Quaternion.Euler(new Vector3(stanceRotationValues[stanceIndex], 0, 0)));
+        transform.SetPositionAndRotation(transform.position, Quaternion.Euler(new Vector3(stanceRotationValues[stanceIndex], transform.eulerAngles.y, transform.eulerAngles.z)));
     }
 }
