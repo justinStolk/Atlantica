d7f8c1a85520ad3f74f5d48d097d6cbe57c6c65d
maxmaxmaxmaxmaxmaxmaxmaxmaxmaxmax <max.nino16@gmail.com>
2022-06-13 17:05:08 +0200

    Heel veel gekloot met een line shader

---
+++ Assets/outline/scripts/Demo_control.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace EasyGameStudio.Jeremy
+{
+    public class Demo_control : MonoBehaviour
+    {
+
+        public AudioSource audio_source;
+        public AudioClip ka;
+
+        public GameObject[] game_objects;
+
+        private int index = 0;
+
+
+        void Start()
+        {
+            this.index = 0;
+        }
+
+
+
+        public void on_previous_btn()
+        {
+            this.index--;
+            if (this.index <= -1)
+                this.index = (this.game_objects.Length - 1);
+
+
+            for (int i = 0; i < this.game_objects.Length; i++)
+            {
+                if (i == this.index)
+                {
+                    this.game_objects[i].SetActive(true);
+                }
+                else
+                {
+                    this.game_objects[i].SetActive(false);
+                }
+            }
+
+            this.audio_source.PlayOneShot(this.ka);
+        }
+
+
+        public void on_next_btn()
+        {
+            this.index++;
+            if (this.index >= this.game_objects.Length)
+                this.index = 0;
+
+            for (int i = 0; i < this.game_objects.Length; i++)
+            {
+                if (i == this.index)
+                {
+                    this.game_objects[i].SetActive(true);
+                }
+                else
+                {
+                    this.game_objects[i].SetActive(false);
+                }
+            }
+
+            this.audio_source.PlayOneShot(this.ka);
+        }
+    }
+}
